51267b39207dbd06d667b635788a18fdad10a87c
Kappa <m1ll3r98@gmail.com>
2026-07-17 16:00:20 +0200

    cambiati paramentri del db, da aggiornare

--- src/main/java/org/generation/italy/demoxchange/DemoXChangeApplication.java
+++ src/main/java/org/generation/italy/demoxchange/DemoXChangeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoXChangeApplication. {
+public class DemoXChangeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoXChangeApplication.class, args);
